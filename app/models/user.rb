6005c7fe6ab7e951a7ea197cd76db0d4bdc0e359
Victor Rudolfsson <>
2020-04-18 18:42:20 +1000

    fix: Added business relation through tiers for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,7 @@
 class User < ApplicationRecord
   include ActiveStorageSupport::SupportForBase64
   belongs_to :tier
+  belongs_to :business, through: :tier
   has_one_base64_attached :avatar
   has_many :visits
   counter_culture :tier, column_name: "user_count"
